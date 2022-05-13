ae48de74c3430332dbf0d91401314d3c27d8afac
UltimateProGrammer <63370104+UltimatePro-Grammer@users.noreply.github.com>
2022-05-12 20:48:33 -0400

    Flying Fish v1.3 Update!

--- src/portfolio/flying-fish/assets/scenes/Scene1.js
+++ src/portfolio/flying-fish/assets/scenes/Scene1.js
@@ -23,23 +23,26 @@ class Scene1 extends Phaser.Scene {
 		const bubble = this.sound.add("bubble");
 		const flap = this.sound.add("flap");
 		const music = this.sound.add("music", { loop: true, volume: 0.8 });
+		music.setVolume(0.3);
 		music.play();
 		this.music = music;
 		this.lastRateScore = 0;
 
 		death = "die";
-		this.add.text(10, 10, "Flying Fish 1.2\nClick and drag to launch\nthe fish.", {fontSize: '15px', fill: '#000', fontFamily: '"Arial"'}).setOrigin(0,0).setDepth(100);
-			var mode = this.add.text(10, 970, "Disable Death", {fontSize: '20px', fill: '#000', fontFamily: '"Arial"'}).setOrigin(0,0).setScrollFactor(0).setDepth(100).setInteractive().on('pointerdown', function() {
-				if(mode.text == 'Disable Death') {
-					mode.text = 'Enable Death'
-					death="nodie";
-				} else if(mode.text == 'Enable Death') {
-					mode.text = 'Disable Death'
-					death="die";
-				}
-			});
+		this.add.text(10, 10, "Flying Fish 1.3\nClick and drag to launch\nthe fish. Avoid the walls!", {fontSize: '15px', fill: '#000', fontFamily: '"Arial"'}).setOrigin(0,0).setDepth(100);
+		const mode = this.add.text(10, 970, "Disable Death", {fontSize: '20px', fill: '#000', fontFamily: '"Arial"'}).setOrigin(0,0).setScrollFactor(0).setDepth(100).setInteractive().on('pointerdown', ()=>{
+			if(mode.text == 'Disable Death') {
+				mode.text = 'Enable Death'
+				death="nodie";
+			} else if(mode.text == 'Enable Death') {
+				mode.text = 'Disable Death'
+				death="die";
+			}
+		});
+		mode.input.cursor = "pointer";
+
 		this.score = 0;
-		this.scoreText = this.add.text(735, 955, this.score, {fontSize: '36px', fill: '#000', fontFamily: '"Arial"'}).setOrigin(1,0).setDepth(100).setScrollFactor(0);
+		this.scoreText = this.add.text(735, 955, this.score, {fontSize: '36px', fill: '#000', fontFamily: '"Arial"', stroke: "#ffffff90", strokeThickness: 5 }).setOrigin(1,0).setDepth(100).setScrollFactor(0);
 		
 		this.startText = this.add.text(375, 500, "Tap to begin!", {fontSize: '30px', fill: '#000', fontFamily: '"Arial"'}).setOrigin(0.5,0.5).setDepth(100);
 		this.startTextScaleIncreasing = true;
@@ -75,11 +78,27 @@ class Scene1 extends Phaser.Scene {
 		}
 		
 		//var win = this.matter.add.image(512, -1600, "win").setScale(1, 1).setIgnoreGravity(true).setCollisionCategory(null);
-		this.graphics = this.add.graphics();
-		this.graphics.lineStyle(1, 0x000000, 1);
+		this.lineGraphics = this.add.graphics();
+		this.lineGraphics.lineStyle(1, 0x000000, 1);
 		this.draw = false;
 		this.drawStart = 0;
-		this.player = this.matter.add.sprite(60, 1100,'fish', null, {timeScale:0.5}).setScale(1.3).setOnCollide(()=>this.lose());
+		this.player = this.matter.add.sprite(60, 1100,'fish', null, {timeScale:0.5}).setScale(1.3).setOnCollide(()=>{
+			if(!this.restartConfig.display) this.lose();
+		});
+
+		this.restartGraphics = this.add.graphics();
+		this.restartConfig = {
+			bgColor: 0xfff000,
+			bgOpacity: 0.5,
+			width: 600,
+			height: 500,
+
+			buttonColor: 0xff0000,
+			buttonWidth: 400,
+
+			display: false
+		};
+
 		//this.matter.overlap(this.player, win, ()=>{alert("You Win!");});
 		this.started = false;
 		//this.pointer = this.input.activePointer;
@@ -87,7 +106,8 @@ class Scene1 extends Phaser.Scene {
 		this.cameras.main.startFollow(this.player);
 		//this.cameras.main.setOrigin(0,1000);
 		this.cameras.main.setBounds(0,-50000,450,51000);
-		this.input.on('pointerdown', function(cursor) {
+		this.input.on('pointerdown', (cursor)=>{
+			if(this.restartConfig.display === true) return;
 			if(this.startText.visible) {
 				this.startText.visible = false;
 			}
@@ -117,7 +137,7 @@ class Scene1 extends Phaser.Scene {
 		
 		
 		this.input.on('pointerup', function(cursor) {
-			if(this.started !== false) {
+			if(this.started === true && this.restartConfig.display === false) {
 				if(cursor.y<960) {
 					this.draw = false;
 					this.player.rotation = Phaser.Math.Angle.BetweenPoints(this.player, this.mouse);
@@ -130,21 +150,79 @@ class Scene1 extends Phaser.Scene {
 				this.player.flipX = true;
 			}
 				
-		}, this)
+		}, this);
+
+		this.input.keyboard.on("keydown-R", (event)=>{
+			if(this.restartConfig.display) {
+				this.restart();
+			}
+		});
+
 		this._create();
 	}
 	
 	lose(force = false) {
 		if((!force && death == "die") || force) {
-			if(confirm("You Lose! Retry?")) {
-				this.music.stop();
-				this.scene.restart();
-			}
+			const bubble = this.sound.add("bubble");
+			bubble.setRate(6);
+			bubble.play();
+			setTimeout(()=>{
+				bubble.setRate(5);
+				bubble.play();
+			}, 100);
+			setTimeout(()=>{
+				bubble.setRate(4);
+				bubble.play();
+			}, 200);
+
+			this.draw = false;
+			this.matter.world.engine.timing.timeScale = 0.5;
+			this.restartConfig.display = true;
+			const {
+				bgColor, bgOpacity, width, height,
+				buttonColor, buttonWidth 
+			} = this.restartConfig;
+			this.add.text(735/2, 350, "Your Score:", {fontSize: '50px', fill: '#000', fontFamily: '"Arial"', stroke: "#fff", strokeThickness: 5 }).setOrigin(0.5).setDepth(100).setScrollFactor(0);
+			this.add.text(735/2, 400, -this.score, {fontSize: '100px', fill: '#fff', fontFamily: '"Arial"', stroke: "#000", strokeThickness: 10 }).setOrigin(0.5, 0).setDepth(100).setScrollFactor(0);
+			this.restartGraphics.fillStyle(bgColor, bgOpacity);
+			this.restartGraphics.fillRoundedRect(375-(width/2), 500-(height/2), width, height, 20).setScrollFactor(0);
+			
+			// restart button
+			const restartButton = this.add.graphics();
+			restartButton.useHandCursor = true;
+			const clickRect = this.add.rectangle(735/2-buttonWidth/2, 555, buttonWidth, 150).setScrollFactor(0).setOrigin(0);
+			restartButton.setInteractive(clickRect, (_clickRect, x, y)=>{
+				return x >= clickRect.getTopLeft().x && x <= clickRect.getTopRight().x
+						&& y <= clickRect.getBottomLeft().y && y >= clickRect.getTopLeft().y;
+			});
+			restartButton.input.cursor = "pointer";
+			// restartButton.setInteractive();
+			restartButton.fillStyle(0x000000, 0.5);
+			restartButton.fillRoundedRect(735/2-(buttonWidth+3)/2, 558, (buttonWidth+3), 150, 20).setScrollFactor(0);
+			const fillButton = ()=>{
+				restartButton.fillStyle(buttonColor, 1);
+			restartButton.fillRoundedRect(735/2-buttonWidth/2, 555, buttonWidth, 150, 20).setScrollFactor(0);
+			};
+			fillButton();
+			restartButton.once("pointerup", ()=>this.restart());
+			const stopShadow = ()=>{
+				restartButton.clear();
+				fillButton();
+			};
+			restartButton.on("pointerdown", stopShadow);
+			const text = this.add.text(735/2, 630, "Restart!", {fontSize: '60px', fill: '#fff', fontFamily: '"Arial"'}).setOrigin(0.5).setDepth(100).setScrollFactor(0).setInteractive().on("pointerup", ()=>this.restart());
+			text.input.cursor = "pointer";
+			text.on("pointerdown", stopShadow);
 		}
+	}
+	restart() {
+		this.matter.world.engine.timing.timeScale = 1;
+		this.music.stop();
+		this.scene.restart();
 	}
 	update() {
 		
-		if(Math.floor(this.player.y) < this.score) {
+		if(!this.restartConfig.display && Math.floor(this.player.y) < this.score) {
 			this.score = Math.floor(this.player.y);
 			this.scoreText.text = -this.score;
 			if(this.lastRateScore - 5000 >= this.score) {
@@ -155,18 +233,21 @@ class Scene1 extends Phaser.Scene {
 		
 		this.mouse.x = this.input.activePointer.x + this.cameras.main.scrollX;
 		this.mouse.y = this.input.activePointer.y + this.cameras.main.scrollY;
+		this.lineGraphics.clear();
 		if(this.draw === true || Date.now() - this.drawStart < 50) {
-			this.graphics.lineStyle(5, 0x000000, 1);
-			this.graphics.lineBetween(this.mouse.x, this.mouse.y, this.player.x, this.player.y);
-			setTimeout(()=>{this.graphics.clear();}, 20);
+			this.lineGraphics.lineStyle(5, 0x000000, 1);
+			this.lineGraphics.lineBetween(this.mouse.x, this.mouse.y, this.player.x, this.player.y);
 		}
 		
 		if(this.started === true) {
-			if(this.player.y>1100) {
-				this.lose(true);
-			} else if(this.player.y < -50000) {
-				alert("You win!");
+			if(!this.restartConfig.display) {
+				if(this.player.y>1100) {
+					this.lose(true);
+				} else if(this.player.y < -50000) {
+					alert("You win!");
+				}
 			}
+			
 			// screenwrap
 			if(this.player.x > 820) {
 				this.player.x = -70;
