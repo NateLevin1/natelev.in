8a10fc5605757f6536cba64de73997349c46685a
NateLevin1 <63370104+NateLevin1@users.noreply.github.com>
2025-01-07 18:31:22 -0500

    ✨ Add initial simple site

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            colors: {
+                link: "#02375B",
+            },
+            fontFamily: {
+                title: ["'Crimson Text'", "serif"],
+                body: ["'Source Serif 4'", "serif"],
+            },
+            animation: {
+                in: "in 0.4s ease-out forwards",
+            },
+
+            keyframes: () => ({
+                in: {
+                    "0%": { opacity: 0, transform: "translateY(1rem)" },
+                    "100%": { opacity: 1 },
+                },
+            }),
+        },
+    },
+    plugins: [require("tailwindcss-animation-delay")],
+};
