de3d2001f2ab9d2dbf91c4042377e6b46035af2e
NateLevin1 <63370104+NateLevin1@users.noreply.github.com>
2025-05-20 00:41:14 -0400

    Add takuan slides redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,8 @@ export default defineConfig({
     output: "static",
     redirects: {
         "/portfolio": "/#my-work",
+        "/portfolio/takuan/slides":
+            "https://docs.google.com/presentation/d/1cc79qzBIWZDFOE1Q0r_E7gS4jlnT_G0ALNRHSyBSiWw/view",
         "/gh": "https://github.com/NateLevin1/",
         "/github": "https://github.com/NateLevin1/",
     },
